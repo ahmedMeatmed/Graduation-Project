102a726fcbd303e948f770d25ff5a8ac7566f288
ahmedMeatmed <ahmedelazzba@gmail.com>
2026-02-12 22:29:53 +0200

    add alerts fetching

--- app/Console/Commands/RedisListener.php
+++ app/Console/Commands/RedisListener.php
@@ -34,28 +34,30 @@ class RedisListener extends Command
         while (true) {
 
             // Wait for ONE log (blocking)
-            $data = Redis::brpop('ids_logs', 0);
+            $logs = Redis::brpop('ids_logs', 0);
+            $alerts = Redis::brpop('ids_alerts', 0);
 
-            $logJson = $data[1];
-            $logs[] = json_decode($logJson, true);
+            $Fetchedlogs[] = json_decode($logs[1], true);
+            $Fetchedalerts[] = json_decode($alerts[1], true);
 
-            $this->info("Received log: " . count($logs));
+            $this->info("Received log: " . count($Fetchedlogs));
+            $this->info("Received log: " . count($Fetchedalerts));
+            print_r($Fetchedalerts);
 
             // When batch is full
-            if (count($logs) >= $batchSize)
-            // if (1)
+            if (count($Fetchedlogs) >= $batchSize)
                 {
 
-                $this->info("Processing 50 logs...");
+                $this->info("Processing 50 Fetchedlogs...");
 
-                foreach ($logs as $log) {
+                foreach ($Fetchedlogs as $log) {
                     // Example: save to DB
                     // LogModel::create($log);
-                    print_r($log);
+                    // print_r($log);
                 }
 
                 // Reset batch
-                $logs = [];
+                $Fetchedlogs = [];
             }
         }
     }
