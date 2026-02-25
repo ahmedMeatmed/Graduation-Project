9e0a1451b112fe2eb822386b16ab35b2bd63a71c
ahmedramadanmain <ahmedramadanmain@gmail.com>
2026-02-26 01:55:39 +0200

    Change IDs From Console To Windows Service

---
+++ IDSWindowsService/Program.cs
@@ -0,0 +1,12 @@
+﻿using IDSWindowsService;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+var builder = Host.CreateDefaultBuilder(args)
+    .UseWindowsService() // <-- مهم جدًا
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await builder.RunAsync();
---
+++ IDSWindowsService/Worker.cs
@@ -0,0 +1,44 @@
+﻿using IDSApp; // ضيف namespace بتاع مشروع الـ IDS Core
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IDSWindowsService
+{
+    public class Worker : BackgroundService
+    {
+        IDSCore idsCore = new IDSCore();
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        // لما الخدمة تبدأ
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Service starting at: {time}", DateTimeOffset.Now);
+            idsCore.Start(); // هنا بنبدأ IDS
+            return base.StartAsync(cancellationToken);
+        }
+
+        // لما الخدمة تتوقف
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Service stopping at: {time}", DateTimeOffset.Now);
+            idsCore.Stop(); // هنا بنوقف IDS
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // loop خفيف يراقب الـ cancellationToken فقط
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
